00c24c9faee9da21b5b1738d2b5239513fb51a22
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-04-21 10:39:56 +0100

    Exercise 03, debug

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -2,10 +2,28 @@
 
 namespace GameSix
 {
-    public class Foe(string name)
+
+    public class Foe(string name){}
     {
         this.name = name; //name oculta this .name
         health = 100;
         shield = 0;
+    }  
+
+    string GetName()
+    {
+        return name;
+    }
+
+    void TakeDamage()
+    {
+        shield -= damage;
+        if (shield < 0)
+        {
+            float damageStillToInflict =-shield;
+            shield = 0;
+            health -= damageStillToInflict;
+            if (health < 0) health = 0;
+        }
     }
 }
