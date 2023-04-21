b423bdc6343303861c21ba0a72b5a4436f001505
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-04-21 13:37:29 +0100

    Exercise 05, Done

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -8,12 +8,12 @@ namespace GameSix
         {
             Console.WriteLine("Please input a number of enemies");
             int N_enemy = Convert.ToInt32(Console.ReadLine());
-            Foe[]enemies;
+            Foe[] enemies = new Foe[N_enemy];
             for (int i = 0; i < N_enemy; i++)
             {
                 Console.WriteLine("Write the enemy's name please");
-                string enemyName = Console.ReadLine();
-                string enemyName = enemies[i];
+                string enemyName =  Console.ReadLine();
+                enemies[i] = new Foe(enemyName);
             }
 
         }
