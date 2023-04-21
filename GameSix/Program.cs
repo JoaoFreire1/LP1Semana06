edca785d8c74fb0879b7e6d88767f60222658f47
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-04-21 10:20:11 +0100

    Exercise 02

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -2,8 +2,10 @@
 
 namespace GameSix
 {
-    public class Foe
+    public class Foe(string name)
     {
-        
+        this.name = name; //name oculta this .name
+        health = 100;
+        shield = 0;
     }
 }
