d07164141220f74a99d77202e00dd69a0c55ebe7
JoaoFreire1 <joao.freire.art@gmail.com>
2025-03-28 14:02:09 +0000

    Construtor

--- MyRPG/GameLevel.cs
+++ MyRPG/GameLevel.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MyRPG
 {
     public class GameLevel
     {
-        
+        int room;
+        Hardness dif;
+        public GameLevel(int room, Hardness dif)
+        {
+            this.room = room;
+            this.dif = dif;
+        }
+        public int GetRoom()
+        {
+            return room;
+        }
     }
 }
